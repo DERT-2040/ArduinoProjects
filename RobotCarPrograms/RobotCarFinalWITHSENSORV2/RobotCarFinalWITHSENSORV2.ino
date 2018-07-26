9ecd6fb56d4d039343e250382378d6317d776883
LeOreo <37230796+LeOreo@users.noreply.github.com>
2018-07-26 11:01:33 -0700

    Add files via upload

--- RobotCarPrograms/RobotCarFinalWITHSENSORV2/RobotCarFinalWITHSENSORV2.ino
+++ RobotCarPrograms/RobotCarFinalWITHSENSORV2/RobotCarFinalWITHSENSORV2.ino
@@ -1,36 +1,34 @@
 // ping sensor
-#include <StandardCplusplus.h>
-#include <vector>
 const int pingPin = 2;
 int dist_in_cm = 100; // pick a high number to start
 int power_turn_level = 255; /* power on turns */
 
 // adjust these till the robot goes streight to compensate for motor differences
 int power_forward_right = 255; 
-int power_forward_left = 255; 
+int power_forward_left = 225; 
 int test_delay = 500;
 
 // motor pins.  N;ote that only pins 2,5,6,9 and 10 can be used for pwm
-int right_forward = 3;
-int right_reverse = 5;
+int right_forward = 5;
+int right_reverse = 3;
 int left_forward = 6;
 int left_reverse = 9;
 
 int echo = 10;
 int trig = 2;
-
-std::vector<double> rollingAverage;
-
+double LPFValue = .55;
+double currentValue = 0;
+double previousValue = 0;
 
 // try this time to make a right turn just above 90 degrees
 int delay_time_ninty_turn = 100;
 // if we are under this distance, make a turn.  For higher power, make this larger
 int cm_for_turn = 35;
-int delay_time_forward = 100;
+int delay_time_forward = 50;
 
 void setup() {
   
-  Serial.begin(9600);
+  //Serial.begin(9600);
   pinMode(pingPin, INPUT);
   
   pinMode(right_forward, OUTPUT);
@@ -52,12 +50,6 @@ void loop() {
     } else {
       move_forward();
     }
-
-
-  
-  Serial.print("distance cm=");
-  Serial.print(dist_in_cm);
-  
 }
 
 void turn_right() {
@@ -83,36 +75,20 @@ int get_distance_cm()
 {
   long duration = 0;
   long cm=0;
-  int average = 0;
-  /*
-  for(int i = 0; i < 4; i++)
-  {
-    duration = 0;
-    digitalWrite(trig, LOW);
-    digitalWrite(trig, HIGH);
-    //delayMicroseconds(1);
-    duration = pulseIn(echo, HIGH);
-    cm += microsecondsToCentimeters(duration);
-  }
-  cm = cm/4;
-  */
+  
   digitalWrite(trig, LOW);
   digitalWrite(trig, HIGH);
+  
   duration = pulseIn(echo, HIGH);
   cm = microsecondsToCentimeters(duration);
-  rollingAverage.push_back(cm);
-  if(rollingAverage.size() > 2)
-  {
-    rollingAverage.erase(rollingAverage.begin()); 
-  }
-  for(int i = 0; i < rollingAverage.size(); i++)
-  {
-    average += rollingAverage[i];  
-  }
-  average = average/rollingAverage.size();
-  Serial.print("average = ");
-  Serial.println(average);
-  return average;
+  currentValue = cm;
+
+  currentValue = currentValue * LPFValue + previousValue * (1-LPFValue);
+  previousValue = currentValue;
+  //Serial.print("currentValue = ");
+  //Serial.println(currentValue);
+  
+  return currentValue;
 }
 
 long microsecondsToCentimeters(long microseconds)
